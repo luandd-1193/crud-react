7572f3f82ccaaaf4f6fa6798f76517e9a2ec7efd
Dang.Dinh.Luan <dang.dinh.luan@framgia.com>
2020-10-05 09:21:50 +0700

    Updates

---
+++ precache-manifest.d1dceb65cc495531e2a3031bdcd831ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7aabc5922995abae9700dd713e8afd",
+    "url": "/crud-react/index.html"
+  },
+  {
+    "revision": "fc91297321adfb565766",
+    "url": "/crud-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "162d5a0605cbd633e485",
+    "url": "/crud-react/static/js/2.cc2334f2.chunk.js"
+  },
+  {
+    "revision": "bfd04eb851fbc8ea43c375cc9d698376",
+    "url": "/crud-react/static/js/2.cc2334f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc91297321adfb565766",
+    "url": "/crud-react/static/js/main.3abb094b.chunk.js"
+  },
+  {
+    "revision": "c399f3c176fd7034a1a8",
+    "url": "/crud-react/static/js/runtime-main.65db3625.js"
+  }
+]);
